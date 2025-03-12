c81a9568d8258e03058885fbe6a821c12d7cfa2a
lucasforlinn <lucasforlinn@gmail.com>
2025-03-12 18:44:30 -0300

    fix: made some changes to follow best practices from cypress

--- cypress/e2e/front.cy.js
+++ cypress/e2e/front.cy.js
@@ -1,42 +1,33 @@
 describe('template spec', () => {
   it('Unsuccessful login', () => {
-    cy.visit('https://front.serverest.dev/login')
-    cy.get('[data-testid="email"]').type('nttteste@gmail.com')
-    cy.get('[data-testid="senha"]').type('123456')
-    cy.get('[data-testid="entrar"]').click()
+    cy.visit('/login')
+
+    cy.login('nttteste@gmail.com', '123456')
     cy.get('.alert').should('have.text', '×Email e/ou senha inválidos')
   })
 
   it('Create a new user', () => {
-    cy.request('GET', 'https://serverest.dev/usuarios?email=ntttdddeste@gmail.com').then((response) => {
-      if (response.body.quantidade > 0) {
-        cy.request('DELETE', `https://serverest.dev/usuarios/${response.body.usuarios[0]._id}`)
-      }
-    })
+    const user = {
+      name: 'Teste',
+      email: 'ntttdddeste@gmail.com',
+      password: '123456'
+    }
 
-    cy.visit('https://front.serverest.dev/cadastrarusuarios')
-    cy.get('[data-testid="nome"]').type('Teste')
-    cy.get('[data-testid="email"]').type('ntttdddeste@gmail.com')
-    cy.get('[data-testid="password"]').type('123456')
-    cy.get('[data-testid="cadastrar"]').click()
+    cy.checkIfUserExistsAndDelete(user.email)
 
+    cy.visit('/cadastrarusuarios')
+
+    cy.registerUser(user.name, user.email, user.password)
     cy.get('.alert-link').should('have.text', 'Cadastro realizado com sucesso')
     cy.get('h1').should('contain.text', 'Serverest Store')
 
   })
 
   it('Search for a product and add it to the shopping list', () => {
-    cy.visit('https://front.serverest.dev/login')
-    cy.get('[data-testid="email"]').type('nttreal@gmail.com')
-    cy.get('[data-testid="senha"]').type('pwd123')
-    cy.get('[data-testid="entrar"]').click()
+    cy.visit('/login')
 
-    cy.get('[data-testid="pesquisar"]').type('logitech')
-    cy.get('[data-testid="botaoPesquisar"]').click()
-    cy.get('[data-testid="adicionarNaLista"]').first().click()
-
+    cy.login('nttreal@gmail.com', 'pwd123')
+    cy.searchProduct('Logitech')
     cy.get('[data-testid="shopping-cart-product-name"]').should('contain.text', 'Logitech')
   })
-
-
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,33 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('[data-testid="email"]').type(email)
+    cy.get('[data-testid="senha"]').type(password)
+    cy.get('[data-testid="entrar"]').click()
+ })
+
+
+Cypress.Commands.add('registerUser', (name, email, password) => {
+    cy.get('[data-testid="nome"]').type(name)
+    cy.get('[data-testid="email"]').type(email)
+    cy.get('[data-testid="password"]').type(password)
+    cy.get('[data-testid="cadastrar"]').click()
+})
+
+Cypress.Commands.add('searchProduct', (product) => {
+    cy.get('[data-testid="pesquisar"]').type(product)
+    cy.get('[data-testid="botaoPesquisar"]').click()
+    cy.get('[data-testid="adicionarNaLista"]').first().click()
+})
+
+Cypress.Commands.add('checkIfUserExistsAndDelete', (email) => {
+    cy.request('GET', `https://serverest.dev/usuarios?email=${email}`).then((response) => {
+        if (response.body.quantidade > 0) {
+          cy.request('DELETE', `https://serverest.dev/usuarios/${response.body.usuarios[0]._id}`)
+        }
+    }) 
+})
 //
 //
 // -- This is a child command --
