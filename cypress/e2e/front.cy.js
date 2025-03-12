380f5e74391095df26748e196ace911698cdb0d8
lucasforlinn <lucasforlinn@gmail.com>
2025-03-12 16:42:07 -0300

    feat: Implementing the create a new user test

--- cypress/e2e/front.cy.js
+++ cypress/e2e/front.cy.js
@@ -7,7 +7,22 @@ describe('template spec', () => {
     cy.get('.alert').should('have.text', '×Email e/ou senha inválidos')
   })
 
-  it('Create a new user', () => {
+  it.only('Create a new user', () => {
+
+    cy.request('GET', 'https://serverest.dev/usuarios?email=ntttdddeste@gmail.com').then((response) => {
+      if (response.body.quantidade > 0) {
+        cy.request('DELETE', `https://serverest.dev/usuarios/${response.body.usuarios[0]._id}`)
+      }
+    })
+
+    cy.visit('https://front.serverest.dev/cadastrarusuarios')
+    cy.get('[data-testid="nome"]').type('Teste')
+    cy.get('[data-testid="email"]').type('ntttdddeste@gmail.com')
+    cy.get('[data-testid="password"]').type('123456')
+    cy.get('[data-testid="cadastrar"]').click()
+
+    cy.get('.alert-link').should('have.text', 'Cadastro realizado com sucesso')
+    cy.get('h1').should('contain.text', 'Serverest Store')
 
   })
 
